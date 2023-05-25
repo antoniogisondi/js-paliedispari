3c72c63befa11ce136f22a520047c861684b4cb6
antoniogisondi <anto.gisondi@tiscali.it>
2023-05-25 18:42:44 +0200

    Finish exercise palindroma

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,1 +1,20 @@
+"use strict"
+let innerWord = prompt("Inserisci una parola")
 
+function reverseWord(word){
+    let reverse = '';
+    for(let i = word.length - 1; i >= 0; i--){
+        reverse += word[i]
+    }
+
+    return reverse;
+}
+
+let reverse_word = reverseWord(innerWord)
+
+if(reverse_word === innerWord){
+    document.getElementById("word").innerHTML = innerWord
+}
+else{
+    alert("La parola è sbagliata. Inseriscine un'altra")
+}
